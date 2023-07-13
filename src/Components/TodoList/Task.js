510baaaa41dd7bfdb4a0efb340d24e5da8efa2d5
Adebimpe96 <sannidebimpe30@gmail.com>
2023-07-13 18:58:05 +0100

    Todo task

---
+++ src/Components/TodoList/Task.js
@@ -0,0 +1,11 @@
+import './task.css';
+const Task = ({ taskName, id, deleteTask, completeTask, completed}) => {
+    return <div className="task" style={{backgroundColor: completed ? "lightBlue" : "", border: "1px solid black"}}>
+            <h2>
+            {taskName}
+            </h2>
+            <button onClick={() => deleteTask(id)}>Remove</button> &nbsp;
+            <button onClick={() => completeTask(id)}>Complete</button>
+            </div>
+}
+export default Task;
--- src/Components/TodoList/Todo.js
+++ src/Components/TodoList/Todo.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Task from "./Task";
+import './todo.css';
 const Todo = () => {
     const [todoList, setTodoList] = useState([]);
     const [newTask, setNewTask] = useState("");
@@ -7,26 +9,42 @@ const Todo = () => {
         setNewTask(e.target.value) 
     };
     const addTask = () => {
-        const newList = [...todoList, newTask];
+        const task = {
+            id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1 ,
+            taskName: newTask,
+            completed: false,
+        };
+        setTodoList([...todoList, task])
+    };
+    const deleteTask = (id) => {
+        const newList = todoList.filter((task) => task.id !== id);
         setTodoList(newList)
     };
+    const completedTask = (id) => {
+const checkCompleted = (todoList.map((task) => {
+    if (task.id === id) {
+        return {...task, completed:true}
+    }else 
+    {
+        return task;
+    }  
+}))
+setTodoList(checkCompleted)
+    }
     return ( 
         <div className="container">
         <div className="addTask">
         <input onChange={handletodoValue}/> &nbsp;
         <button onClick={addTask}> Add Task</button>
-        <div>
-        {todoList.map((task) => {
-            return <div>
-            <h2>
-            {task}
-            </h2>
-            </div>
-        })}</div>
-       
         </div>
 
-        <div className="list"></div>
+        <div className="list">
+        {todoList.map((task) => {
+            return(
+                <Task taskName={task.taskName} id={task.id} deleteTask={deleteTask} completed ={task.completed} completeTask={completedTask}/>
+            )   
+        })}
+        </div>
         </div>
      );
 }
